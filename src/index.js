ebf961e9db9414d409073e8e14051ea6546d2e21
Dmitri9149 <dmitri.as.name@gmail.com>
2020-01-02 17:44:49 +0200

    apollo client is added

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { ApolloProvider } from "@apollo/react-hooks"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+  })
+  
+  ReactDOM.render(
+    <ApolloProvider client={client} >
+      <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+  )
