beae1130c22e69bab380211a9bd52c62d8887b24
Dmitri9149 <dmitri.as.name@gmail.com>
2020-01-03 19:31:07 +0200

    8.11

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -3,42 +3,78 @@ import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 
-const Authors = ({show, authors}) => {
-  if (!show) {
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  if (!props.show) {
     return null
   }
 
-  if (authors.loading) {
+  if (props.authors.loading) {
     return <div>loading...</div>
   }
 
+  const submit = async (e) => {
+    e.preventDefault()
+    await props.editAuthor({
+      variables:{name:name , setBornTo:born}
+    })
 
+    console.log('add author...')
+
+    setName('')
+    setBorn('')
+  } 
 
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.data.allAuthors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                born
+              </th>
+              <th>
+                books
+              </th>
             </tr>
-          )}
-        </tbody>
-      </table>
-
+            {props.authors.data.allAuthors.map(a =>
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2>Set birthday</h2>
+        <form onSubmit = {submit}>
+          <div>
+            name
+            <input 
+              value = {name}
+              onChange = {({target})=>setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+              type = 'number'
+              value = {born}
+              onChange = {({target})=> setBorn(parseInt(target.value))}
+            />
+          </div>
+          <button type='submit'>update author</button>
+        </form>   
+      </div>
     </div>
+  
   )
 }
 
