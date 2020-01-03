889ea589d542d89d13b4faf3af44a1cb8c574594
Dmitri9149 <dmitri.as.name@gmail.com>
2020-01-03 15:45:56 +0200

    useApolloClient is eliminated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import ApolloClient from 'apollo-boost'
-import { useApolloClient } from '@apollo/react-hooks'
 import { ApolloProvider } from "@apollo/react-hooks"
 
 const client = new ApolloClient({
