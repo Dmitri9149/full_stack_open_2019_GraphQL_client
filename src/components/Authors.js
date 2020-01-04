10760d20de7e77f4ee64771008e7a5739fe2989b
Dmitri9149 <dmitri.as.name@gmail.com>
2020-01-04 15:44:59 +0200

    unneeded code is eliminated

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -42,15 +42,6 @@ const Authors = (props) => {
 
   })
 
-  const Countries = [
-    { label: "Albania", value: 355 },
-    { label: "Argentina", value: 54 },
-    { label: "Austria", value: 43 },
-    { label: "Cocos Islands", value: 61 },
-    { label: "Kuwait", value: 965 },
-    { label: "Sweden", value: 46 },
-    { label: "Venezuela", value: 58 }
-  ];
 
   return (
     <div>
