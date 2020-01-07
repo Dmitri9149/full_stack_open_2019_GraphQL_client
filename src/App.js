a7395977966575500af5c5f2786ebef1a7289005
Dmitri9149 <dmitri.as.name@gmail.com>
2020-01-07 12:08:48 +0200

    initial commit LoginForm + import of the Form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { gql } from 'apollo-boost'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
